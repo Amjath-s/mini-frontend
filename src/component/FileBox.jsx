ebad32ce982f582ab086484648ff1f46da911f80
Amjath s <145754786+Amjath-s@users.noreply.github.com>
2024-04-05 01:24:21 +0530

    Add files via upload

--- src/component/FileBox.jsx
+++ src/component/FileBox.jsx
@@ -1,113 +1,116 @@
+
+
 import { useEffect, useState } from "react";
 import { PrevDoc } from "./PrevDoc";
 
 
 
-export function FileBox()
+export function FileBox() {
+  const [selectedFile, setSelectedFile] = useState(null);
+  const [uploadedFile, setUploadedFile] = useState(false);
+  const [submitbuttonclicked, setSubmitbuttonclicked] = useState(false);
+  const [responsefetched, Setresponsefetched] = useState(false)
 
-{
-    const [selectedFile,setSelectedFile]=useState(null);
-    const [uploadedFile,setUploadedFile]=useState(false);
+
+  const handlefilechange = (file) => {
+    setSelectedFile(file);
+    // console.log('file is ',selectedFile)
+
+  };
+  // useEffect(()=>{
+  //   console.log("selected file",selectedFile)
+
+  // },[submitbuttonclicked])
+
+  const handlefileupload = async (file) => {
+    if (file) {
+      const formdata = new FormData;
+      formdata.append('file', file);
+      try {
+        const response = await fetch('https://api.ocr.space/parse/image', {
+          method: 'POST',
+          body: formdata,
+          headers: {
+            'apikey': 'K85586486088957'
+          },
 
 
-    const handlefilechange=(file)=>{
-      setSelectedFile(file);
-      // console.log('file is ',selectedFile)
-    
-    };
-    // useEffect(()=>{
-    //   console.log("selected file",selectedFile)
-
-    // },[selectedFile])
-
-    const handlefileupload = async(file)=>{
-      if(file){
-        const formdata =new FormData;
-        formdata.append('file',file);
-        try{
-          const response=await fetch('https://api.ocr.space/parse/image',{
-            method:'POST',
-            body:formdata,
-            headers:{
-              'apikey':'K85586486088957'
-            },
-            
-            
-          });
-          if(response.ok){
-            const data=await response.json();
-            console.log("ocr resulr",data);
-            setUploadedFile(true)
-          }
-          else{
-            console.error('error',response.statusText);
-          }
+        });
+        if (response.ok) {
+          const data = await response.json();
+          console.log("ocr resulr", data);
+          setUploadedFile(true)
+          Setresponsefetched(true)
         }
-        catch(error){
-          console.error('error',error)
+        else {
+          console.error('error', response.statusText);
         }
       }
-       else{
-        console.log('no file seleced');
-       }
-       console.log("gjhgh",selectedFile) 
-       
-        
-      };
-      const handleclick=(event)=>
-      {
-        const file=event.target.files[0];
-        handlefilechange(file);
-      };
-      const handleDrop=(event)=>
-      {
-        event.preventDefault();
-        // const files = event.dataTransfer.files;
-        // if (files.length > 0) {
-        //   const file = files[0];
-        //   handlefilechange(file);
-        //}
-     
-      };
-      const handleDrag=(event)=>{
-        event.preventDefault();
-        const file=event.traget.files[0]
-        handlefilechange(file);
+      catch (error) {
+        console.error('error', error)
+      }
+    }
+    else {
+      console.log('no file seleced');
+    }
+    console.log("gjhgh", selectedFile)
 
-      };
-      const handleupload=()=>{
-        handlefileupload(selectedFile);
-        
 
-      };
+  };
+  const handleclick = (event) => {
+    const file = event.target.files[0];
+    handlefilechange(file);
+  };
+  const handleDrop = (events) => {
+    events.preventDefault();
+
+    const files = events.dataTransfer.files;
+    if (files.length > 0) {
+      const file = files[0];
+      handlefilechange(file);
+    }
+
+
+  };
+  const handledragover=(event)=>{
+    event.preventDefault();
+
+  }
+  const handleupload = () => {
+    handlefileupload(selectedFile);
+    setSubmitbuttonclicked(true);
+  };
   
-     
-    
-    
-    return(
-        <>
-        <div className="drag-drop" 
-        onDrop={handleDrop}
-        onDragOver={handleDrag}>
-          <p className="paragraph"> Drag and Drop File here  </p>
-          <label htmlFor="file-upload" > Click To Select the File   </label>
-         
-          <input type="file"  onChange={handleclick} className="upload"  id="file-upload"></input>
-          
-        </div>
-       { selectedFile &&   (<button onClick={handleupload} className="submit"> submit</button>  
-        )}  
-        
-          {uploadedFile  && <PrevDoc selectedFile={selectedFile}></PrevDoc>} 
-         
-      
-      
-     
-        
-        
-        
-        </>
-        
-    );
-}
+  return (
+    <> 
 
+    <div className="maindiv">
+
+    
+    <div className="prevfile">  
+    recent file viewed
+    {responsefetched&&submitbuttonclicked&&uploadedFile&& <PrevDoc selectedFile={selectedFile}></PrevDoc>} 
+
+    </div>
+                            
+      <div className="drag-drop"
+        onDrop={handleDrop}
+        onDragOver={handledragover}>
+
+              
+        <p className="paragraph"> Drag and Drop File here  </p>
+       
+        <label htmlFor="file-upload" > Click To Select the File   </label>
+       
+        <input type="file" onChange={handleclick} className="upload" id="file-upload"></input>
+
+      </div>
+      </div>
+     
+      {selectedFile && (<button onClick={handleupload} className="submit"> submit</button>
+      )}
+        {/* {responsefetched&&submitbuttonclicked&&uploadedFile&& <PrevDoc selectedFile={selectedFile}></PrevDoc>} */}
+    </>
+
+  );
+}
