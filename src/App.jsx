423b3c5c96f3eb49a63e0a12b4e6c5f92b892d16
Amjath s <145754786+Amjath-s@users.noreply.github.com>
2024-04-11 01:03:46 +0530

    routing and styling done

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,39 @@
 import { Header } from "./component/Header"
 import './App.css'
 import { FileBox } from "./component/FileBox"
-
-
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Result } from "./component/Result";
+import { Routes,Route} from "react-router-dom";
 function App() {
 
 
   return (
     <>
-      <Header> </Header>
-      <FileBox></FileBox>
+    <div className="app">
+      <Header/>
+  
+      
+      
+    <Routes>
+        <Route path="/" element={<FileBox />} />
+        <Route path="/fetchdata" element={<Result/>} />
+        
+      </Routes>
+
+      
+   
+     
+
+
+
+
+      {/* <Header></Header>
+       <FileBox></FileBox>
+           */}
+       {/* <Header> </Header>
+      <FileBox></FileBox> */}
+     
+    </div>
     
   
     </>
